7baac8edf63e05aab1640655f502ebc8ac192a8d
s-maarouf <maarouf.salahedd@gmail.com>
2024-01-06 23:35:12 +0100

    Updated storage engine methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,20 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        if cls is None or id is None:
+            return None
+        all_objs = self.all(cls)
+        for obj in all_objs.values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
