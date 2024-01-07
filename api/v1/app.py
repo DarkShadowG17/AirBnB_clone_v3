df0516561f886ab064883ebebbfe47bbb43e5309
s-maarouf <maarouf.salahedd@gmail.com>
2024-01-07 17:01:26 +0100

    Hot fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-
-import os
-from flask import Flask
-from models import storage
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
 from api.v1.views import app_views
-from os import getenv
+from flask import Flask
+from flasgger import Swagger
+from models import storage
+import os
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+swagger = Swagger(app)
 
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_appcontext(error):
-    """Closes storage"""
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
     storage.close()
 
 
 if __name__ == "__main__":
-    """main function"""
+    """
+    MAIN Flask App
+    """
     app.run(host=host, port=port, threaded=True)
