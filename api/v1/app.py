3c836be4e47af43a8f55a21e1254289ad699045c
s-maarouf <maarouf.salahedd@gmail.com>
2024-01-08 00:29:23 +0100

    Implemented cors instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,14 @@ from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
